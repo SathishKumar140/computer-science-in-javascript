a91683f6d6b640ec62ceec10ae212b8e260b5316
Ankeet Maini <ankeet.maini@gmail.com>
2017-01-16 00:34:26 +0530

    Adds delete node in BST

--- src/ds/BinarySearchTree.js
+++ src/ds/BinarySearchTree.js
@@ -6,7 +6,7 @@ function createNode (data) {
   };
   // trying to be fancy :P
   return Object.create(Node, {
-    value: { writable: false, configurable: true, value: data },
+    value: { writable: true, configurable: true, value: data },
   });
 }
 
@@ -22,11 +22,8 @@ function BinarySearchTree () {
       return;
     }
     // left is already assigned, find new place
-    if (node.value > rootNode.left.value) {
-      setRight(rootNode.left, node);
-      return;
-    }
-    setLeft(root.left, node);
+    if (node.value > rootNode.left.value) return setRight(rootNode.left, node);
+    return setLeft(root.left, node);
   }
 
   function setRight (rootNode, node) {
@@ -34,11 +31,8 @@ function BinarySearchTree () {
       rootNode.right = node;
       return;
     }
-    if (node.value > rootNode.right.value) {
-      setRight(rootNode.right, node);
-      return;
-    }
-    setLeft(rootNode.right, node);
+    if (node.value > rootNode.right.value) return setRight(rootNode.right, node);
+    return setLeft(rootNode.right, node);
   }
 
   function insert (data) {
@@ -48,15 +42,71 @@ function BinarySearchTree () {
       return;
     }
     const currentNode = root;
-    if (node.value < currentNode.value) {
-      setLeft(currentNode, node);
-      return;
+    if (node.value < currentNode.value) return setLeft(currentNode, node);
+    return setRight(currentNode, node);
+  }
+
+  function getMax (rootNode = root) {
+    let currentNode = rootNode;
+    while (currentNode.right) currentNode = currentNode.right;
+    return currentNode;
+  }
+
+  function getMin (rootNode = root) {
+    let currentNode = rootNode;
+    while (currentNode.left) currentNode = currentNode.left;
+    return currentNode;
+  }
+
+  function find (value) {
+    // using an inner function because I want to use
+    // recursion, I <3 it!
+    function findInTree (rootNode, v = value) {
+      if (v === rootNode.value) return true;
+      if (v < rootNode.value) return findInTree(rootNode.left, v);
+      return findInTree(rootNode.right, v);
     }
-    setRight(currentNode, node);
+    return findInTree(root, value);
+  }
+
+  function remove (value) {
+    // using an inner function because I want to use
+    // recursion, because I couldn't think of anything else :/
+    function removeNode (rootNode, v) {
+      if (v === rootNode.value) {
+        // case: no children, easy peasy
+        if (!rootNode.left && !rootNode.right) return null;
+
+        // case: one child
+        if (!rootNode.left) return rootNode.right;
+        if (!rootNode.right) return rootNode.left;
+
+        // case: two children, trickier
+        // choose the smallest element from the
+        // right subtree and replace with the parent node
+        const tempNode = getMin(rootNode.right);
+        rootNode.value = tempNode.value;
+        rootNode.right = removeNode(rootNode.right, tempNode.value);
+        return rootNode;
+      }
+      // go right subtree
+      if (v > rootNode.value) {
+        rootNode.right = removeNode(rootNode.right, v);
+        return rootNode;
+      }
+      // go to left subtree
+      rootNode.left = removeNode(rootNode.left, v);
+      return rootNode;
+    }
+    root = removeNode(root, value);
   }
 
   return {
     insert,
+    getMax,
+    getMin,
+    find,
+    remove,
     get () {
       return root;
     },
--- src/ds/__tests__/BinarySearchTree-test.js
+++ src/ds/__tests__/BinarySearchTree-test.js
@@ -1,6 +1,7 @@
 import expect from 'expect';
 
 import createBinarySearchTree from '../BinarySearchTree';
+import InOrder from '../../algorithms/tree-traversals/InOrderTraversal';
 
 describe('BinarySearchTree', () => {
   let bst;
@@ -35,5 +36,66 @@ describe('BinarySearchTree', () => {
     expect(output.left.right.value).toBe(7);
     expect(output.right.left.value).toBe(11);
     expect(output.right.right.value).toBe(100);
+  });
+
+  it('returns the max value', () => {
+    bst.insert(8);
+    bst.insert(3);
+    bst.insert(12);
+    bst.insert(1);
+    bst.insert(7);
+    bst.insert(11);
+    bst.insert(100);
+    const output = bst.getMax();
+    expect(output.value).toBe(100);
+  });
+
+  it('returns the min value', () => {
+    bst.insert(8);
+    bst.insert(3);
+    bst.insert(12);
+    bst.insert(1);
+    bst.insert(7);
+    bst.insert(11);
+    bst.insert(100);
+    const output = bst.getMin();
+    expect(output.value).toBe(1);
+  });
+
+  it('finds a value', () => {
+    bst.insert(8);
+    bst.insert(3);
+    bst.insert(12);
+    bst.insert(1);
+    bst.insert(7);
+    bst.insert(11);
+    bst.insert(100);
+    const output = bst.find(1);
+    expect(output).toBe(true);
+  });
+
+  it('deletes a node just fine', () => {
+    bst.insert(50);
+    bst.insert(30);
+    bst.insert(70);
+    bst.insert(20);
+    bst.insert(40);
+    bst.insert(60);
+    bst.insert(80);
+    expect(InOrder(bst.get())).toEqual(
+      [20, 30, 40, 50, 60, 70, 80],
+    );
+    bst.remove(20);
+    expect(InOrder(bst.get())).toEqual(
+      [30, 40, 50, 60, 70, 80],
+    );
+    bst.remove(30);
+    expect(InOrder(bst.get())).toEqual(
+      [40, 50, 60, 70, 80],
+    );
+    bst.remove(70);
+    expect(InOrder(bst.get())).toEqual(
+      [40, 50, 60, 80],
+    );
   });
 });
